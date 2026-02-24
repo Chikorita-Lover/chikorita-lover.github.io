f354a0ef0c7e03f9e3e59d4445d28e08674eca50
Chikorita Lover <cjgardels@gmail.com>
2026-02-24 15:17:56 -0600

    Initialize Vue app
    Web site begins now!
    Also changed the favicon

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import SiteHeader from './components/SiteHeader.vue'
+</script>
+
+<template>
+  <SiteHeader />
+
+  <main>
+    <p>
+      Hello world!
+    </p>
+  </main>
+</template>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header id="header">
+    <a href="/">
+      <img alt="Vue logo" class="logo" src="/src/assets/logo.png" width="125" height="125" />
+    </a>
+
+    <div>
+      <h1>Roosey's House</h1>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+}
+
+header#header {
+  border-bottom: 1px solid var(--color-border);
+  padding: 1rem;
+  width: 100%;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
