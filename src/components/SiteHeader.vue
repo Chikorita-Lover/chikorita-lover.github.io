4a5ccd93ba2c9d39d0d3a7fe7e1786176dde4b14
Chikorita Lover <cjgardels@gmail.com>
2026-02-25 13:18:10 -0600

    Adjust site header
    Site header now has a max width, appearing smaller on large displays
    Alt text for logo changed
    Display has been improved for vertical layouts

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,11 +1,12 @@
 <template>
   <header id="header">
-    <a href="/">
-      <img alt="Vue logo" class="logo" src="/src/assets/logo.png" width="125" height="125" />
-    </a>
-
-    <div>
-      <h1>The Library of Roosey</h1>
+    <div id="logo">
+      <a href="/">
+        <img alt="Site logo" class="logo" src="/src/assets/logo.png" width="100" height="100" />
+      </a>
+      <div>
+        <h1>The Library of Roosey</h1>
+      </div>
     </div>
   </header>
 </template>
@@ -14,19 +15,20 @@
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
+  line-height: 1.0;
+}
+
+div#logo {
+  display: flex;
+  place-items: center;
+  margin: auto;
+  width: 100%;
+  max-width: 1280px;
 }
 
 header#header {
   border-bottom: 1px solid var(--color-border);
   padding: 1rem;
   width: 100%;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 }
 </style>
